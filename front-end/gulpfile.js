7af8c28aa7362356a310ccd9961f8925ea431f31
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-17 12:42:51 -0600

    sass/css files setup

--- front-end/gulpfile.js
+++ front-end/gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var sass = require("gulp-sass");
+var watch = require("gulp-watch");
 var nodemon = require('gulp-nodemon');
+
+
+// write a task that will convert a .scss file to a .css file.
+gulp.task("compile-sass", function() {
+    gulp.src("./public/stylesheets/style.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("./public/stylesheets/"))
+});
+gulp.task("watch-sass", function() {
+    gulp.watch(["./public/stylesheets/style.sass"],["compile-sass"]);
+});
 
 // nodemon it up!
 gulp.task('Nodemon', restartServer);
@@ -11,4 +24,4 @@ function restartServer() {
     });
 };
 
-gulp.task('default', ['Nodemon']);
+gulp.task('default', ['Nodemon','watch-sass']);
