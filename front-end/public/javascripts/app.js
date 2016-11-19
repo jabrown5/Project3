50fb4ee7e0f8857f86f29685a7a575b2b93ef320
Traci Williams <traciwilliams@Tracis-MacBook-Pro.local>
2016-11-19 13:10:25 -0600

    form for login and registration

--- front-end/public/javascripts/app.js
+++ front-end/public/javascripts/app.js
@@ -1,1 +1,27 @@
 console.log('THE PAGE IS LOADED!');
+
+
+angular.module('potty', [ 'ngRoute'
+]).config([
+  '$locationProvider',
+  '$routeProvider',
+  function($locationProvider, $routeProvider) {
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false // good for anything IE9+
+    })
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'ngViews/register.html',
+        controller: 'RegisterCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'ngViews/login.html',
+        controller: 'LoginCtrl'
+      })
+
+    $routeProvider.otherwise({ redirectTo: '/' });
+  }
+]);
