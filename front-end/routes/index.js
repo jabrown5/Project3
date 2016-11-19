285b47c26b345a20e562e374ca20abcbbc078afd
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-18 20:36:34 -0600

    front-end restroom & comment submission speaking to server

--- front-end/routes/index.js
+++ front-end/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The PottyCheck' });
 });
 
-// RENDER THE 'REGISTER A NEW USER' PAGE
-router.get('/register', function(req, res, next) {
-    res.render('register', {});
-});
+// // RENDER THE 'REGISTER A NEW USER' PAGE
+// router.get('/register', function(req, res, next) {
+//     res.render('register', {});
+// });
 
-// RENDER THE 'RETURNING USER LOGIN' PAGE
-router.get('/login', function(req, res, next) {
-    res.render('login', {});
-});
+// // RENDER THE 'RETURNING USER LOGIN' PAGE
+// router.get('/login', function(req, res, next) {
+//     res.render('login', {});
+// });
 
-// RENDER THE 'ADD A NEW POTTY TO THE DB' PAGE
-router.get('/potty', function(req, res, next) {
-    res.render('potty', {});
-});
+// // RENDER THE 'ADD A NEW POTTY TO THE DB' PAGE
+// router.get('/potty', function(req, res, next) {
+//     res.render('potty', {});
+// });
 
-// RENDER THE 'ADD A COMMENT' PAGE
-router.get('/comments', function(req, res, next) {
-    res.render('comments', {});
-});
+// // RENDER THE 'ADD A COMMENT' PAGE
+// router.get('/comments', function(req, res, next) {
+//     res.render('comments', {});
+// });
 
 module.exports = router;
