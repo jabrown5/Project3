1993912f7ab6813b9bd2376d91821eb3d6ff2f8e
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-20 16:21:28 -0600

    timestamps working, foreign key set table set up

--- back-end/models/restroom.rb
+++ back-end/models/restroom.rb
@@ -1,5 +1,9 @@
 class Restroom < ActiveRecord::Base 
 # (self.table_name = 'restroom') we don’t need this bc it already knows 
-  self.table_name = 'restroom'
+  self.table_name = 'restrooms'
 
+  has_many :restroomcomments #has many rows in the the restroom comments table
+  has_many :comments, through: :restroomcomments
+  	# looking at comments table, there are MANY comments,
+  	# connections are made via the "restroomcomments" table
 end
