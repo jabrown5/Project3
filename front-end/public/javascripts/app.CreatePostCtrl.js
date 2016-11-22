135ca0b5c786ccefdb33731cb057d15b395fa631
chitownhope <apr_dec@hotmail.com>
2016-11-22 16:47:11 -0600

    login and register forms mostly worked out...potty check logo wraps href in nav bar

--- front-end/public/javascripts/app.CreatePostCtrl.js
+++ front-end/public/javascripts/app.CreatePostCtrl.js
@@ -5,17 +5,20 @@ angular.module('pottyCheck')
             'thanks for your post. view the blog to see it live'
         ];
         $scope.message = $scope.messages[0];
-        $scope.createPost = function(facility_name, address, publicly_accessible, cleanliness, family_friendly, gender) {
+        $scope.createPost = function(facility_name, address, latitude, longitude, publicly_accessible, cleanliness, family_friendly, gender, user_submission) {
             $http({
                 url: 'http://localhost:9292/api/restroom/',
                 method: 'POST',
                 params: {
                     facility_name: facility_name, 
                     address: address, 
+                    latitude: latitude,
+                    longitude: longitude,
                     publicly_accessible: publicly_accessible, 
                     cleanliness: cleanliness, 
                     family_friendly: family_friendly, 
-                    gender: gender}
+                    gender: gender,
+                    user_submission: user_submission}
             }).success(function (results) {
                 $scope.message = $scope.message[1];
             }).error(function (err) {
