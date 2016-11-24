fdfb09df771ce05295fe1cd7379d5ad8bf88c599
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-23 21:07:56 -0600

    styles merged, views merged

--- front-end/public/javascripts/app.js
+++ front-end/public/javascripts/app.js
@@ -13,10 +13,10 @@ angular.module('pottyCheck', [
         })
 
         $routeProvider
-            // .when('/', {
-            //     templateUrl: 'ngViews/home.html',
-            //     controller: 'PostsCtrl'
-            // })
+            .when('/', {
+                templateUrl: 'ngViews/admin.html',
+                // controller: 'AdminCtrl' // NEEDS FIXING !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            })
             .when('/home', {
                 templateUrl: 'ngViews/home.html',
                 controller: 'HomeCtrl'
