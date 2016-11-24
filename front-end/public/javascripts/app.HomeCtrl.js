07e70dc6bf85b47f810b884863b617ca8ebc799d
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-23 22:46:43 -0600

    no errors in console, BUT map still not rendering

--- front-end/public/javascripts/app.HomeCtrl.js
+++ front-end/public/javascripts/app.HomeCtrl.js
@@ -4,23 +4,13 @@ console.log('HOME CTRL LINKED');
 angular.module('pottyCheck')
     .controller('HomeCtrl', function($scope, $http, $rootScope) {
 
-       // console.log('hello')
 //this is our map
-      
-  var mapOptions = {
-      zoom: 12,
-      center: new google.maps.LatLng ({lat: 41.88425, lng: -87.63245}),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-
-  $scope.map = new google.maps.Map(document.getElementById('map')), mapOptions;
-
-  //$scope.markers = [];
-
-
-
-
-
+$scope.initMap = function () {
+       map = new google.maps.Map(document.getElementById('map'), {
+         center: {lat: -34.397, lng: 150.644},
+         zoom: 8
+       });
+     }
 });
 
 
