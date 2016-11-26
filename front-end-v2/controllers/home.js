2cf7b1685edf5941d67cee31fb32f74b16d26f8d
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-26 12:49:23 -0600

    adding ng work

--- front-end-v2/controllers/home.js
+++ front-end-v2/controllers/home.js
@@ -1,14 +1,17 @@
-let express = require('express'),
-  router = express.Router();
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'The PottyCheck' })
 })
 
-router.get('/map', function(req, res, next) {
-  res.render('map', { })
-})
+// router.get('/map', function(req, res, next) {
+//   res.render('map', { })
+// })
 
+// router.get('./public/ngViews/login.html', function(req, res, next) {
+//   res.render('login', { })
+// })
 
 module.exports = router
--- front-end-v2/public/javascripts/app.js
+++ front-end-v2/public/javascripts/app.js
@@ -25,6 +25,61 @@
 //       } 
 
 
+console.log('THE POTTYCHECK PAGE IS LOADED!');
+
+angular.module('pottyCheck', [
+    'ngRoute'
+]).config([
+    '$locationProvider',
+    '$routeProvider',
+    function($locationProvider, $routeProvider) {
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false // good for anything IE9+
+        })
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'ngViews/admin.html',
+                // controller: 'MapCtrl' // NEEDS FIXING !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            })
+            .when('/home', {
+                templateUrl: 'ngViews/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/register', {
+                templateUrl: 'ngViews/register.html',
+                controller: 'RegisterCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'ngViews/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/create', {
+                templateUrl: 'ngViews/create.html',
+                controller: 'CreatePostCtrl'
+            })
+            .when('/comments', {
+                templateUrl: 'ngViews/comments.html',
+                controller: 'CommentsCtrl'
+            })
+            .when('/find', {
+                templateUrl: 'ngViews/find.html',
+                controller: 'FindCtrl'
+            });
+            // .when('/admin', {
+            //     templateUrl: 'ngViews/admin.html',
+            //     controller: 'DeletePostCtrl'
+            // });
+
+        $routeProvider.otherwise({ redirectTo: '/' });
+    }
+]);
+
+
+
+
 //one marker with info window and you are here info window
 window.onload = function initFirstMap(){
         var pottymap = new google.maps.Map(document.getElementById('firstmap'), {
