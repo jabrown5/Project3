552d2c44bd4bb73a944a3e357a79c875d4049571
Jennifer Brown <j.a.brown.5@gmail.com>
2016-11-28 10:50:19 -0600

    working on ajax call

--- front-end-v2/public/javascripts/app.HomeCtrl.js
+++ front-end-v2/public/javascripts/app.HomeCtrl.js
@@ -3,11 +3,16 @@ angular.module('pottyCheck')
 
         $scope.posts = [];
 
+        // var locationName = ;
+        // var geolocation = ;
+        // var comments = ;
+
         $scope.fetch = function() {
             $http.get('http://localhost:9292/api/restroom/').success(function (results) {
             // $http.get('http://localhost:9292/api/restroom/3/comments').success(function (results) {
-                $scope.posts = results
-                console.log(results)
+                $scope.posts = [results[0].facility_name, results[0].latitude, results[0].longitude, results[0].cleanliness]
+                //console.log(results[0].facility_name, results[0].latitude, results[0].longitude, results[0].cleanliness)
+                console.log($scope.posts)
             }).error(function(err) {
                 console.log('Fetch failed; it didn\'t happen');
                 console.log(err);
